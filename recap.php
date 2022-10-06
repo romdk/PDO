91745364bfa136b58e50f675e1c0790ae4a6083b
romdk <drack.roman67@gmail.com>
2022-10-06 14:57:41 +0200

    affichage du total

--- recap.php
+++ recap.php
@@ -27,17 +27,22 @@
                         "</tr>",
                     "</thead>",
                     "<tbody>";
-
-            foreach($_SESSION['products'] as $index => $product){
-                echo "<tr>",
-                        "<td>".$index."</td>",
-                        "<td>".$product['name']."</td>",
-                        "<td>".number_format($product['price'],2,",","&nbsp;")."&nbsp;€</td>",
-                        "<td>".$product['qtt']."</td>",
-                        "<td>".number_format($product['total'],2,",","&nbsp;")."&nbsp;€</td>",
-                    "</tr>";
-            }
-            echo "</tbody>",
+                        $totalGeneral = 0;
+                        foreach($_SESSION['products'] as $index => $product){
+                            echo "<tr>",
+                                    "<td>".$index."</td>",
+                                    "<td>".$product['name']."</td>",
+                                    "<td>".number_format($product['price'],2,",","&nbsp;")."&nbsp;€</td>",
+                                    "<td>".$product['qtt']."</td>",
+                                    "<td>".number_format($product['total'],2,",","&nbsp;")."&nbsp;€</td>",
+                                "</tr>";
+                            $totalGeneral += $product['total'];
+                        }
+                        echo "<tr>",
+                                "<td colspan=4>Total général : </td>",
+                                "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                            "</tr>",            
+                    "</tbody>",
                 "</table>";
         }
     
