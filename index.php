c065b2828f3b7a677d7c47d781362a825f7d9fa6
romdk <drack.roman67@gmail.com>
2022-10-25 15:41:42 +0200

    ajout index

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
+include 'db-functions.php';
+connection();
+findAll($pdo);
+
+foreach($products as $product){
+    ?><p><?php echo "<a href=''>".$product['name']."</a><br><br>".$product['description']."<br><br>".$product['price']."<br><br><a href=''>Ajouter au painer</a><br><br>"; ?></p><?php
+}      
+
 
 ?>
