ad2e0ad4f1e012fa9ff26eab710fe7d0ff96b3d3
romdk <drack.roman67@gmail.com>
2022-10-26 16:59:43 +0200

    empecher doublon

--- traitement.php
+++ traitement.php
@@ -9,12 +9,13 @@
 
     switch($action) {
 
-        case "addProduct";
-        $product = findOneById($pdo,$id);
-        foreach($products as $product){
-            $name = $product['name'];
-            $price = $product['price'];
-            $qtt = 1;
+        case "ajouterPanier";
+        
+        findOneById($pdo,$id);
+        $name = $product['name'];
+        $price = $product['price'];
+        $qtt = 1;
+        $id = $product['ID'];
         
         $product = [
             "name" => $name,
@@ -23,32 +24,35 @@
             "total" => $price*$qtt
         ];
 
-        }
-        $_SESSION['products'][] = $product ;
-        header("Location:recap.php");
+// echo ($_SESSION['products'][$id]['name']);
+// echo $name;
+
+// foreach($_SESSION['products'] as ){
+if(empty($_SESSION['products'][$id]) || $_SESSION['products'][$id]['name'] !== $name){
+    $_SESSION['products'][] = $product ;
+    header("Location:index.php");
+    affMsgAjout($name);
+}
+if($_SESSION['products'][$id]['name'] == $name){
+    $_SESSION['products'][$id]['qtt']++  ;
+    header("Location:index.php");
+    affMsgQttUp();
+}
+// }    
+
         break;
 
-        case "ajouterProduit";
+        case "creerProduit";
 
         if(isset($_POST['submit'])){
-
-            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
-            $price = filter_input(INPUT_POST,"price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-            $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
-
-            if($name && $price && $qtt){
-
-                $product = [
-                    "name" => $name,
-                    "price" => $price,
-                    "qtt" => $qtt,
-                    "total" => $price*$qtt
-                ];
-
-                $_SESSION['products'][] = $product;
-            }
             
-            header("Location:index.php");
+            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+            $descr = filter_input(INPUT_POST, "description", FILTER_SANITIZE_SPECIAL_CHARS);
+            $price = filter_input(INPUT_POST,"price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+            
+            insertProduct($pdo,$name,$descr,$price);
+            $id = $pdo->lastInsertId();
+            header("Location:product.php?id=$id");
             
             if($price == 0){
                 affMsgErrPrix();
@@ -58,7 +62,7 @@
                 affMsgErrProduit();
             }
             else{
-                affMsgAjout($name);  
+                affMsgCreer($name);  
             }
             
         }
@@ -92,4 +96,8 @@
         header("Location:recap.php");
         break;
     }
+
+    // session_destroy();
+
+    
 ?>
