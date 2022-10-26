ffe41f04d01ef8e905dea7f1c406e5d1740d3011
romdk <drack.roman67@gmail.com>
2022-10-26 09:34:37 +0200

    affichage 50 characteres description

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ findAll($pdo);
 
 foreach($products as $product){
     $id = $product['ID'];
-    ?><p><?php echo "<a href=product.php?id=$id>".$product['name']."</a><br>".$product['description']."<br>".$product['price']."&euro;<br><a href='traitement.php'>Ajouter au panier</a><br>"; ?></p><?php
+    ?><p><?php echo "<a href=product.php?id=$id>".$product['name']."</a><br>".mb_strimwidth($product['description'], 0, 50, "...")."<br>".$product['price']."&euro;<br><a href='traitement.php'>Ajouter au panier</a><br>"; ?></p><?php
 }      
 
 
